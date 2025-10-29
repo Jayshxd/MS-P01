f363d66b12812afd4333f574cb462b369ea44569
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-29 23:04:16 +0530

    feat : implemented load balancer

--- Product-Service/src/main/java/com/jayesh/productservice/controller/ProductController.java
+++ Product-Service/src/main/java/com/jayesh/productservice/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.jayesh.productservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/test")
     public String get() {
-        return "Your order IPHUNEEEEEEEE is placed";
+        String port = environment.getProperty("server.port");
+        return "Your order IPHUNEEEEEEEE is placed"+" *** this server running on port -> "+port;
     }
 }
